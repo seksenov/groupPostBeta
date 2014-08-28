cad343f9a6c25d49ac15998ac37bd7a53d1154dc
seksenov <seksenovk@gmail.com>
2014-08-28 14:09:05 -0700

    FB stuff

--- js/mainScript.js
+++ js/mainScript.js
@@ -22,11 +22,11 @@ userTable=client.getTable("userTable");
 getPostIts();
 
 //do the FB init stuff
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+//function checkLoginState() {
+//    FB.getLoginStatus(function(response) {
+//      statusChangeCallback(response);
+//    });
+//  }
 
 
 
