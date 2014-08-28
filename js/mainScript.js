22c91c8db309b1b8f1b066bb56c059b942294286
seksenov <seksenovk@gmail.com>
2014-08-28 09:54:57 -0700

    loading the FB SDK on the second page

--- js/mainScript.js
+++ js/mainScript.js
@@ -54,6 +54,16 @@ function checkLoginState() {
   });
 
   };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  
 //Get the user FB uid of the person logged in
 FBuid();
 
