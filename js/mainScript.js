708e368d5ad77b00e86291a93c880f2612db2f27
seksenov <seksenovk@gmail.com>
2014-09-03 09:54:39 -0700

    adding the overflow manager

--- js/mainScript.js
+++ js/mainScript.js
@@ -153,9 +153,9 @@ function selectDiv(divID, buttonID)
     div.contentEditable = 'true';
     cursorManager.setEndOfContenteditable(div);
     div.focus();
-    editable.on('input', function() {
-      return filter_newlines(div);
-    });
+    //editable.on('input', function() {
+      //return filter_newlines(div);
+    //});
     button.innerHTML = 'Post';
     //FBuid();
   }
@@ -176,9 +176,9 @@ function selectDiv(divID, buttonID)
       addPostIt(false, "");
     }
 
-    editable.on('input', function() {
-      return filter_newlines(div);
-    });
+    //editable.on('input', function() {
+    //  return filter_newlines(div);
+    //});
     div.style.backgroundColor = '#FFFF99';
     div.contentEditable = 'false'; 
     button.innerHTML = 'Edit';
