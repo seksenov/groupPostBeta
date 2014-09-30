9cce31d1fa10504358125c199d5a0855d88077b0
seksenov <seksenovk@gmail.com>
2014-09-30 14:29:52 -0700

    adding skeleton in order to change post it colors

--- js/mainScript.js
+++ js/mainScript.js
@@ -477,6 +477,7 @@ function getPostIts(){
 
   query.where({ uid: userID }).read().then(function (postIts) {
     console.log("Yo the number of PostITs is: " + postIts.length);
+    console.log("THis is what the first post it says: " + postIts[0].PostItNote);
     
     for (var i = 0; i < postIts.length; i++) {
       console.log(postIts[i].PostItNote);
